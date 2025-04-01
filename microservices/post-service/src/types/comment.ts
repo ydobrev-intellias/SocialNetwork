f0e0769e9e290b07d65c189db805394dbc0afa1d
Yavor Dobrev <yavor.dobrev@intellias.com>
2025-04-01 12:57:16 +0300

    feat(post-service): update services and types for posts and comments

--- microservices/post-service/src/types/comment.ts
+++ microservices/post-service/src/types/comment.ts
@@ -1,3 +1,13 @@
+import { Comment } from '../entities/Comment';
+
 export interface CreateOrUpdateCommentSchema {
   content: string;
 }
+export type CommentsWithOwnerProfile = Comment & {
+  ownerProfile?: {
+    username: string;
+    email: string;
+    id: string;
+    avatarPath: string;
+  };
+};
--- microservices/post-service/src/types/post.ts
+++ microservices/post-service/src/types/post.ts
@@ -1,3 +1,4 @@
+import { Post } from '../entities/Post';
 import { PostPrivacy } from './common';
 
 export interface CreatePostSchema {
@@ -9,3 +10,15 @@ export interface UpdatePostSchema {
   content?: string;
   privacy?: PostPrivacy;
 }
+export type PostWithOwnerProfile = Post & {
+  ownerProfile?: { id: string; username: string; email: string; avatarPath: string };
+};
+
+export type RepostWithOwnerProfile = Post & {
+  ownerProfile?: { id: string; username: string; email: string; avatarPath: string };
+  originalPost: PostWithOwnerProfile;
+};
+
+export type PostWithNestedOwnerProfile = PostWithOwnerProfile & {
+  originalPost?: PostWithOwnerProfile;
+};
