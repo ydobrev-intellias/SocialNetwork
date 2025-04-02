22247054189c276003603952eb8216d05493bca6
Yavor Dobrev <yavor.dobrev@intellias.com>
2025-04-02 13:30:43 +0300

    feat(user-service): add follow service implementation

---
+++ microservices/user-service/src/services/follow.service.ts
@@ -0,0 +1,74 @@
+import { Context } from 'koa';
+import { AppDataSource } from '../data-source';
+import { User } from '../entities/User';
+import { Follow } from '../entities/Follow';
+
+export const follow = async (ctx: Context) => {
+  const { userId } = ctx.params;
+  const userHeaders = ctx.headers['x-auth-user-data'];
+  const userRepository = AppDataSource.getRepository(User);
+  const user = await userRepository.findOne({ where: { id: userId } });
+  if (!user) {
+    ctx.throw(400, 'User does not exist');
+  }
+  if (!userHeaders) {
+    ctx.throw(401, 'Unauthorized');
+  }
+  const { id: followerId } = JSON.parse(userHeaders as string);
+
+  if (followerId === userId) {
+    ctx.throw(403, 'Forbidden from following yourself');
+  }
+
+  const followRepository = AppDataSource.getRepository(Follow);
+  const existingFollow = await followRepository.findOne({
+    where: { follower: { id: followerId }, following: { id: userId } },
+  });
+
+  if (existingFollow) {
+    ctx.throw(400, 'You are already following this user');
+  }
+  const followerUser = await userRepository.findOne({ where: { id: followerId } });
+  if (!followerUser) {
+    ctx.throw(400, 'Follower does not exist');
+  }
+
+  const follow = new Follow();
+
+  follow.follower = followerUser;
+  follow.following = user;
+
+  await followRepository.save(follow);
+};
+
+export const unfollow = async (ctx: Context) => {
+  const { userId } = ctx.params;
+  const userHeaders = ctx.headers['x-auth-user-data'];
+
+  const userRepository = AppDataSource.getRepository(User);
+  const user = await userRepository.findOne({ where: { id: userId } });
+  if (!user) {
+    ctx.throw(400, 'User does not exist');
+  }
+  if (!userHeaders) {
+    ctx.throw(401, 'Unauthorized');
+  }
+  const { id: followerId } = JSON.parse(userHeaders as string);
+
+  console.log('Follower and user id', { followerId, userId });
+
+  if (followerId === userId) {
+    ctx.throw(403, 'Forbidden from unfollowing yourself');
+  }
+
+  const followRepository = AppDataSource.getRepository(Follow);
+  const existingFollow = await followRepository.findOne({
+    where: { follower: { id: followerId }, following: { id: userId } },
+  });
+
+  if (!existingFollow) {
+    ctx.throw(400, 'You are not following this user');
+  }
+
+  await followRepository.remove(existingFollow);
+};
